83c03401f9cffd48ffb1ccd6417f755718944dbc
picardcharlie <picardcharlie@gmail.com>
2021-09-07 14:22:25 -0500

    section 6 labs

---
+++ 06_advanced-python-concepts/06_03_remove_messages.py
@@ -0,0 +1,12 @@
+# You just want to print out the name of the soup, but there's also this
+# other message printing out when you run this script!
+# Make the necessary edits in `codingnomads/cook.py` to avoid that extra
+# print statement. You can't remove any code in `cook.py`,
+# but you can add code.
+
+from codingnomads.cook import soup
+
+print(f"I like {soup}.")
+
+
+# added if __name__ == "__main__": to cook.py
